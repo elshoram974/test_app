36b27c7b326ac28b7e35d373b1ce516719741fd4
Mohammed El Shora <riyadm2001@gmail.com>
2025-01-11 21:55:19 +0200

    Add initial localization and routing setup
    - Added initial localization files for English (`intl_en.arb`, `messages_all.dart`, `messages_en.dart`, `l10n.dart`).
    - Added initial routing configuration (`app_routes.dart`).
    - Updated `main.dart` to remove default Flutter demo code and set up the app title.

---
+++ lib/core/config/routes/app_routes.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+
+class AppRoutes {
+  static const String home = '/';
+  static const String metrics = '/metrics';
+  static const String graph = '/graph';
+
+  static List<GetPage> get pages => [];
+}
